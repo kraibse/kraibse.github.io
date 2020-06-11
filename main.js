7659c704d4aa596579f8f65b6b653730aa492b03
rrleo <leontechno4@gmail.com>
2020-06-11 12:17:35 +0200

    Automated table of contents

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+// get 'top'-button
 btn = document.getElementById("top-button");
 window.onscroll = function() {onScrolling()};
 
-function onScrolling() {
+// setup for table of contents
+setupTOC();
+
+function onScrolling()
+{
     if (document.body.scrollTop > 240 || document.documentElement.scrollTop > 240) {
         btn.style.display = "block";
     }
@@ -10,7 +15,86 @@ function onScrolling() {
     }
 }
 
-function gotoTop() {
+function gotoTop()
+{
     document.body.scrollTop = 0; //Safari
     document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+}
+
+// creating table of contents
+function setupTOC()
+{
+    var div = document.getElementById('navigation');
+    createHeading(div);
+    var categoryList = createObject("ul", div, "id", "categoryList");
+
+    // get categories
+    var categories = document.getElementsByClassName('container main');
+    for (var i = 0; i < categories.length; i++)
+    {
+        // name and id creation
+        var current = categories[i];
+        var categoryName = current.getElementsByClassName('heading')[0].innerHTML;
+        var name = categoryName.toLowerCase();
+
+        // generation of lists and sub categories
+        var category = createObject("li", categoryList, "id", "li-" + name);
+        var title = createObject("a", category, "href", "#" + name);
+        title.innerHTML = categoryName;
+
+        var itemList = createObject("ul", category, "id", "ul-" + name);
+
+        var div = current.getElementsByClassName('list')[0];
+        var subs = div.getElementsByTagName('h3');
+        var ids = generateHeadingIds(div, subs, name);
+
+        var titles = [];
+        for (j = 0; j < subs.length; j++)
+        {
+            titles.push(subs[j].innerHTML);
+        }
+
+        for (var itemIndex = 0; itemIndex < ids.length; itemIndex++)
+        {
+            var li = createObject("li", itemList, "class", itemIndex);
+            var a = createObject("a", li, "href", "#" + ids[itemIndex]);
+            a.innerHTML = titles[itemIndex];
+            
+            li.appendChild(a);
+            itemList.appendChild(li);
+        }
+        categoryList.appendChild(itemList);
+    }
+}
+
+function createHeading(parent)
+{
+    var heading = document.createElement("h2");
+    heading.innerHTML = "Table of contents:";
+    parent.appendChild(heading);
+}
+
+function createObject(type, parent, tag, tagValue)
+{
+    var object = document.createElement(type);
+    object.setAttribute(tag, tagValue);
+    parent.appendChild(object);
+
+    return object;
+}
+
+function generateHeadingIds(parent, headings, name)
+{
+    var subs = parent.getElementsByClassName('sub');
+    var ids = [];
+
+    for (var i = 0; i < subs.length; i++)
+    {
+        var text = headings[i].innerHTML;
+        text = text.split(" ")[0].toLowerCase();
+
+        headings[i].setAttribute("id", name + "-" + text);
+        ids.push(headings[i].getAttribute("id"));
+    }
+    return ids;
 }
