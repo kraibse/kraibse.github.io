13c86b236f955b7ae19a02e5a6b711275925f1ec
kraibse <leontechno4@gmail.com>
2022-04-05 11:04:43 +0200

    Added autofill feature on full row/col

--- nonogram/board.js
+++ nonogram/board.js
@@ -1,6 +1,7 @@
 class Board
 {
     grid = [];
+    revealedTiles = [];
     indicators = [[], []];
 
     size = 10;
@@ -8,53 +9,81 @@ class Board
     totalPoints = 0;
     points = 0;
 
-    isSetting = true;
+    isCommenting = false;
     
     constructor(_size) {
         this.size = _size;
 
-        this._generate();
+        this._generateGrid();
         this._generateIndicators();
     }
     
-    _fill() {
+    _fill(x, y, _mode=false) {
         // check if the last checked element in row / col
-        // 
 
-        var pointsX = map[y-1].reduce((a, b) => a + b, 0);
-        var col = [];
-        map.forEach(element => {
-            col.push(element[x - 1]);
+        // var pointsY = this._getRow(x, this.revealedTiles)
+        // .reduce((a, b) => a + b, 0);
+
+        // var pointsX = this._getRow(y, this.revealedTiles)
+        // .reduce((a, b) => a + b, 0);
+
+        var rowHints = this.indicators[1][y]
+        .reduce((a, b) => a + b, 0);
+
+        var colHints = this.indicators[0][x]
+        .reduce((a, b) => a + b, 0);
+
+
+        var pointsX = this.revealedTiles[y]
+        .filter((e) => {
+            return e == 1;
         })
+        .reduce((a, b) => a + b, 0);
+
+        var col = [];
+        this.revealedTiles.forEach(element => {
+            col.push(element[x]);
+        })
+        var pointsY = col
+        .filter((e) => {
+            return e == 1;
+        })
+        .reduce((a, b) => a + b, 0);
         
-        var pointsY = col.reduce((a, b) => a + b, 0);
-        
-        // compilation of revealed tiles per row / col
-        
-        for (var x = 1; x < map.length + 1; x++)
+        for (var i = 0; i < this.size; i++)
         {
+            this.isCommenting = true;
             var element = document.getElementById(x + '_' + y);
             
-            if (element.inner_html == ' ' && map[y-1][x-1] == 0)
-            {
-                // not revealed yet
-                
+            if (rowHints == pointsX) {
+                if (this.revealedTiles[y][i] == 0) {
+                    this.reveal(i, y, false);
+                }
+            }
+            if (colHints == pointsY) {
+                if (this.revealedTiles[i][x] == 0) {
+                    this.reveal(x, i, false);
+                }
             }
         }
+        this.isCommenting = _mode;
     }
     
-    _generate(_size=this.size) {
+    _generateGrid(_size=this.size) {
         // Returns an two-dimensional array directly proportional to _size
         console.log("Randomizing grid");
         for (var y=0; y < _size; y++)
         {
             this.grid.push([]);
+            this.revealedTiles.push([]);
+
             for (var x=0; x < _size; x++)
             {
                 var r = Math.floor(Math.random() * 2);  // generate 0 or 1
                 this.grid[y].push(r);
+                this.revealedTiles[y].push(0);
                 
-                if (r == 1) { this.points++; }
+                if (r == 1) { this.totalPoints++; }
             }
         }
         console.log(this.grid);
@@ -66,23 +95,10 @@ class Board
         for (var i = 0; i < this.size; i++)
         {
             // filter out lengths from non-bomb tiles from row array
-            var row = this.grid[i].join()
-            .replace(/,/g, "").split("0")                         // ['','','','1','1','','','1''1''1']
-            .filter((value, index, arr) => {    // removing empty strings from array
-                return value != "";
-            });
-            
+            var row = this._getRow(i, this.grid);
+
             // retreive all values on column with x = i
-            var col = [];
-            for (var _y = 0; _y < this.size; _y++) {
-                col.push(this.grid[_y][i]);
-            }
-            // filter out bomb tiles from column array
-            col = col.join()
-            .replace(/,/g, "").split("0")
-            .filter((value, index, arr) => {    
-                return value != "";
-            });
+            var col = this._getColumn(i, this.grid)
             
             // adding up the ones per element to actual length of free space
             for (var l = 0; l < row.length; l++) {
@@ -97,6 +113,37 @@ class Board
         }
 
         console.log(this.indicators);
+    }
+
+    _getColumn(_x, map) {
+        /*
+            Retreives the live tiles in a specific column from the generated map.
+        */
+        var col = [];
+        for (var _y = 0; _y < this.size; _y++) {
+            col.push(map[_y][_x]);
+        }
+        // filter out bomb tiles from column array
+        col = col.join()
+        .replace(/,/g, "").split("0")
+        .filter((value, index, arr) => {    
+            return value != "";
+        });
+
+        return col;
+    }
+
+    _getRow(_y, map=this.grid) {
+        /*
+            Retreives the live tiles in a specific row from the generated map.
+        */
+        var row = map[_y].join()
+        .replace(/,/g, "").split("0")       // ['','','','1','1','','','1''1''1']
+        .filter((value, index, arr) => {    // removing empty strings from array
+            return value != "";
+        });
+
+        return row;
     }
 
     build() {
@@ -149,7 +196,7 @@ class Board
                     {
                         hintlist += this.indicators[1][y][i] + " ";
                     }
-
+                    
                     p.innerHTML = hintlist;
                     td.appendChild(p);
                 }
@@ -158,6 +205,7 @@ class Board
                     // set button for tile
                     var btn = document.createElement("button");
                     btn.setAttribute("id", x + "_" + y);
+                    btn.setAttribute("class", "shadow tile");
                     btn.setAttribute("onclick", "board.reveal(" + x + "," + y + ")");
                     
                     // btn.innerHTML = map[y - 1][x - 1].toString();//" ";
@@ -173,9 +221,15 @@ class Board
     
     }
     
-    reveal(x, y) {
-        console.log(x, y);
+    enableCommenting() {
+        this.isCommenting = !this.isCommenting;
+        var btn = document.getElementById("switch");
 
+        if (this.isCommenting) { btn.innerHTML = "Disable Commenting"; }
+        else { btn.innerHTML = "Enable Commenting"; }
+    }
+
+    reveal(x, y, _isFilling=false) {
         if (this.lives == 0)
         {
             return; // GAME OVER
@@ -185,27 +239,23 @@ class Board
         var color = "#000";
         var bg = "";
     
-        if (this.isSetting)
-        {
+        if (this.isCommenting == false) {
             var content = this.grid[y][x].toString();
-            if (tile.innerHTML == " ")
-            {
-                if (content == 0)
-                {
+            if (tile.innerHTML == ' ') {
+                if (content == 0) {
                     document.getElementById('livesCounter').innerHTML = --this.lives;
                     bg = "#ff726f";
                     color = bg;
                     content = '💣';
+                    this.revealedTiles[y][x] = -1;
                 }
                 else
                 {
-                    this.points += 1;
+                    this.points++;
                     bg = "; background-color: #90EE90;";
                     color = "#fff";
                     content = "✔"
-                    // content = '🟢';
-                    
-                    // fillRemainingRow(x, y);
+                    this.revealedTiles[y][x] = 1;
                 }
             }
             else
@@ -217,16 +267,18 @@ class Board
         {
             var comment_symbol = '?';
     
-            if (tile.innerHTML == comment_symbol)
+            if (tile.innerHTML == comment_symbol && !_isFilling)
             {
                 var content = " ";
                 bg = "#fff";
+                this.revealedTiles[y][x] = 0;
             }
             else if (tile.innerHTML == " ")
             {
                 var content = comment_symbol;
                 color = "#666";
                 bg = "#ccc";
+                this.revealedTiles[y][x] = 2;
             }
             else 
             {
@@ -234,23 +286,23 @@ class Board
             }
         }
         
-        tile.setAttribute("class", "shadow tile");
         tile.setAttribute("style", "color: " + color + "; background-color: "+ bg);
         tile.innerHTML = content;
-    
+        
         if (this.points == this.totalPoints)
         {
             var pgm = document.getElementById("postgameMessage");
             pgm.innerHTML = "YOU WON!!";
             pgm.setAttribute("style", "display: block; color: #0f0");
         }
-        else if (this.lives == 0)
+        
+        if (this.lives == 0)
         {
             var pgm = document.getElementById("postgameMessage");
             pgm.innerHTML = "GAME OVER";
             pgm.setAttribute("style", "display: block; color: rgb(255, 204, 36)");
         }
-        else {}
 
+        if (!_isFilling) { this._fill(x, y, this.isCommenting); }
     }
 }
