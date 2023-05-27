a53f6abca84e8e220c67e8ab15b8eac0928ea22f
kraibse <33869366+kraibse@users.noreply.github.com>
2023-05-27 12:15:24 +0200

    style(css): add progress bar to navbar to indicate page scroll progress feat(js): add progress bar functionality to show page scroll progress style(css): remove unnecessary whitespace in styles.css file

---
+++ js/progressbar.js
@@ -0,0 +1,9 @@
+$(window).on('scroll', function() {
+    var windowHeight = $(window).height();
+    var documentHeight = $(document).height();
+    var scrollTop = $(window).scrollTop();
+    var progress = (scrollTop / (documentHeight - windowHeight)) * 100;
+    
+    $('.progress-bar').css('width', progress + '%');
+  });
+  
